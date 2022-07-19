ff27c88c26eb10eb15c0e3fdd0c5ae3c149c374b
emrecellebi <Emre_celebi_199505@hotmail.com>
2022-07-19 21:46:05 +0300

    .

---
+++ nortwind-redux/src/components/categories/CategoryList.js
@@ -0,0 +1,20 @@
+import {Component} from "react";
+import {connect} from "react-redux";
+
+class CategoryList extends Component {
+	render(){
+		return (
+			<div>
+				<h3>Categories</h3>
+			</div>
+		);
+	}
+}
+
+function mapStateToProps(state){
+	return {
+		currentCategory: state.changeCategoryReducer
+	}
+}
+
+export default connect(mapStateToProps)(CategoryList);
